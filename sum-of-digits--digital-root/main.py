284a697ab067622bfd2194b1c3a05d5224aa1a1e
Felix <felixbinder123@gmx.de>
2023-02-06 10:54:28 +0100

    Update main.py

--- sum-of-digits--digital-root/main.py
+++ sum-of-digits--digital-root/main.py
@@ -1,16 +1,12 @@
 # https://www.codewars.com/kata/541c8630095125aba6000c00/train/python
 
 def digital_root(n):
-
     while len(str(n)) > 1:
         n = get_sum(n)
-
     return n
 
 def get_sum(n):
     sum = 0
-
     for i in str(n):
         sum += int(i)
-
     return sum
